3685b1a47b2876d56186d4becab338fded091fa6
shruti chawla <114687114+Shrutichawla786@users.noreply.github.com>
2024-02-11 17:53:45 +0530

    Day11

---
+++ Day11.cpp
@@ -0,0 +1,12 @@
+191.Number of 1 Bits
+link->https://leetcode.com/problems/number-of-1-bits/description/
+code->int hammingWeight(uint32_t n) {
+        int count=0;
+        while(n!=0){
+            if(n&1){
+                count=count+1;
+            }
+           n= n>>1;
+        }
+        return count;
+    }
